e0007d333375fcc5b97523e728feee33fbeb3907
chaudhuree <chaudhuree00@gmail.com>
2023-10-20 12:04:24 +0600

    created pages and route them in the app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import ForgotPassword from "./pages/ForgotPassword";
+import Home from "./pages/Home";
+import Offers from "./pages/Offers";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 function App() {
-
   return (
     <>
-      
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/offers" element={<Offers />} />
+        </Routes>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

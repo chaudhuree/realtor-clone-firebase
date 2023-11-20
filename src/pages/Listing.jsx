149f9804bcc9d3b376e3240f837210ad6b13f54d
chaudhuree <chaudhuree00@gmail.com>
2023-11-21 01:23:23 +0600

    2️⃣3️⃣ Add share button to Listing page

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -3,7 +3,9 @@ import { useState } from "react";
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import Spinner from "../components/Spinner";
+// app firebase components
 import { db } from "../firebase";
+//swiper js
 import { Swiper, SwiperSlide } from "swiper/react";
 import{
   Autoplay,
@@ -12,11 +14,14 @@ import{
   Pagination,
 } from "swiper/modules";
 import "swiper/css/bundle";
+//icons
+import { FaShare } from "react-icons/fa";
 
 export default function Listing() {
   const params = useParams();
   const [listing, setListing] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [shareLinkCopied, setShareLinkCopied] = useState(false);
  
   useEffect(() => {
     async function fetchListing() {
@@ -56,6 +61,26 @@ export default function Listing() {
           </SwiperSlide>
         ))}
       </Swiper>
+      {/*
+        share button
+      */}
+      <div
+        className="fixed top-[13%] right-[3%] z-10 bg-white cursor-pointer border-2 border-gray-400 rounded-full w-12 h-12 flex justify-center items-center"
+        onClick={() => {
+          navigator.clipboard.writeText(window.location.href);
+          setShareLinkCopied(true);
+          setTimeout(() => {
+            setShareLinkCopied(false);
+          }, 2000);
+        }}
+      >
+        <FaShare className="text-lg text-slate-500" />
+      </div>
+      {shareLinkCopied && (
+        <p className="fixed top-[23%] right-[5%] font-semibold border-2 border-gray-400 rounded-md bg-white z-10 p-2">
+          Link Copied
+        </p>
+      )}
     </main>
   );
 }
